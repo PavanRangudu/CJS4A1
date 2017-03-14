6fa15fb25dec75681514e45b7306247009e726f7
PavanRangudu <pavanharishankar@gmail.com>
2017-03-14 23:27:04 +0530

    method_overridden_access_specifiers

---
+++ src/CJS4A2_child.java
@@ -0,0 +1,31 @@
+
+class Parent{
+
+	private void method1(){
+		System.out.println ("Parent's method1()");
+	}
+
+	public void method2() {
+		System.out.println ("Parent's method2()");
+		method1();
+	}
+}
+
+public class CJS4A2_child extends Parent {
+	
+	public void method1() {
+		System.out.println ("Child's method1()");
+	}
+
+		
+	public static void main(String args[]) {
+		Parent p = new CJS4A2_child();
+		p.method2();
+	}
+}
+
+/* Outcome : 
+
+C. Prints : Parent's method2()
+			Parent's method1()
+*/
